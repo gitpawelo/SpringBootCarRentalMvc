c24730784b2691bf609f86f88798e6100fd973ca
gitpawelo <gitpawelo@wp.pl>
2019-08-25 16:35:18 +0200

    add CarService implementation

--- src/main/java/com/mycompany/interfaces/CarService.java
+++ src/main/java/com/mycompany/interfaces/CarService.java
@@ -1,4 +1,15 @@
 package com.mycompany.interfaces;
 
+import com.mycompany.model.Car;
+
+import java.util.List;
+
 public interface CarService {
+
+    List<Car>getAllCars();
+    Car addCar(Car car);
+    Car updateCar(Car car);
+    void deleteCar(Car car);
+
+
 }
